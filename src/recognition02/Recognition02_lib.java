c1e869a913ac5a57a365f86c4a25d6bcdad30c4d
1618058 <1618058@cs16058>
2018-11-01 16:19:51 +0900

    課題２

--- src/recognition02/Recognition02_lib.java
+++ src/recognition02/Recognition02_lib.java
@@ -14,6 +14,8 @@ import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
 
 
 
+
+
 public class Recognition02_lib {
 	VisualRecognition service;
 	IamOptions iamOptions = null;
@@ -21,7 +23,7 @@ public class Recognition02_lib {
 		service = new VisualRecognition("2018-03-19");
 		
 		IamOptions iamOptions = new IamOptions.Builder()
-				  .apiKey("1618058")
+				  .apiKey("Y_DgzKGp6VUf0dyIgKpuKnBlTL8lDUUYiVuBJra2Jk6D")
 				  .build();
 				service.setIamCredentials(iamOptions);
 	}
@@ -60,7 +62,11 @@ public void getJson(DetectedFaces result) {
 		gender = 1;
 	}
 	double gender_score = node.get("images").get(0).get("faces").get(0).get("gender").get("score").doubleValue();
+	mySQL mysql = new mySQL();
+	mysql.updateImage(age_min,age_max,age_score,gender,gender_score);
+	
 	 System.out.println("age_min : " + age_min);
 	
 }
+
 }
